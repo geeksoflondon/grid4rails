6b08a41d9509c0a1f6752b8396a2c4aa3e083e27
Tom Morris <tom@tommorris.org>
2011-07-05 10:24:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Griddy::Application.config.secret_token = '3ed510801f58608f57f7298ef8854ba37a00ca85f74e8f996351117495d026c4c62833348e1209067439f485bf579c140416fdd69b68d76d326427bd63408953'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Griddy::Application.config.session_store :cookie_store, :key => '_griddy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Griddy::Application.config.session_store :active_record_store
