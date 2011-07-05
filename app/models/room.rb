94cc6bdffa824e442d1cbfb9468c7eb060344187
Tom Morris <tom@tommorris.org>
2011-07-05 11:15:28 +0100

    added letter conversion

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,14 @@
 class Room < ActiveRecord::Base
+  def letter_code
+    # this generates a letter code - so the ID 0 would return 'a' etc.
+    n = @id.to_i
+    # this is currenly zero indexed. uncomment next line to change
+    #n = n - 1
+    letter_code = n.to_i.to_s(27).tr("0-9a-q", "a-z").downcase
+    return letter_code
+  end
+  
+  def self.letter_code_to_id(str)
+    str.tr("a-z", "0-9a-q").upcase.to_i(27)
+  end
 end
--- test/unit/room_test.rb
+++ test/unit/room_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def test_letter_conversion
+    room = Room.new
+    room.name = "Charing Cross"
+    room.description = "A railway station, of course."
+    room.save
+    assert(room.letter_code.class == String)
   end
 end
