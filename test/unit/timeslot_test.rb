059afaf7c12c4f3a3b256f2ab534fc0300cf701b
Tom Morris <tom@tommorris.org>
2011-07-22 14:52:33 +0100

    written tests for the logic of timeslot creation
    written fixtures to go with them.

--- test/unit/timeslot_test.rb
+++ test/unit/timeslot_test.rb
@@ -1,8 +1,30 @@
 require 'test_helper'
 
 class TimeslotTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "load fixtures from file" do
+    assert timeslots(:one).name == "1"
+    assert timeslots(:one).start < timeslots(:one).end
+    assert timeslots(:one) != timeslots(:two)
+    assert timeslots(:one).class == timeslots(:two).class
+  end
+
+  test "cannot create duplicate timeslots" do
+    ts1 = Timeslot.new do |t|
+      t.name  = "one"
+      t.start = Date.parse("2011-07-22 14:00:00")
+      t.end   = Date.parse("2011-07-22 14:30:00")
+    end
+
+    ts1.save
+    assert ts1.errors.empty?
+
+    ts2 = Timeslot.new do |t|
+      t.name  = "two"
+      t.start = Date.parse("2011-07-22 14:00:00")
+      t.end   = Date.parse("2011-07-22 14:30:00")
+    end
+
+    assert ts2.save == false
+    assert !(ts2.errors.empty?)
   end
 end
