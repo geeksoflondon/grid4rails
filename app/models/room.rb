8751ca34be6f9331b92cbbef3e0b9058ebc32db0
Kevin Prince <nextgengames@gmail.com>
2011-07-24 12:47:10 +0100

    Added validation to make sure values are present.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ActiveRecord::Base
+  
+  validates_presence_of :name, :description, :short_code, :weighting 
+  
   validates_uniqueness_of :short_code, :on => :create, :message => "must be unique"
   validates_format_of :short_code, :with => /^[A-Za-z0-9]{1,5}$/, :on => :create, :message => "is not of the correct format. It must be between one and five letters and numbers without spaces or symbols."
   
