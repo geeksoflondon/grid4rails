d80bdb1c1483f95f555ff1b57077f3882b8326dc
martian-a <coder@bluegumtree.com>
2011-07-26 16:01:56 +0100

    8: Redirect home page to default grid view http://github.com/geeksoflondon/grid4rails/issues/issue/8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Griddy::Application.routes.draw do
+  get "home/index"
+
   resources :talks
 
   resources :users
@@ -60,7 +62,7 @@ Griddy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
