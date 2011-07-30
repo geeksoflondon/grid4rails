daa9d60fe8a3e4808243ffe15a1d35a4a921bbf3
Kevin Prince <nextgengames@gmail.com>
2011-07-30 15:04:27 +0100

    Added lunch, dinner etc to the fixtures

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,7 +4,7 @@ class Room < ActiveRecord::Base
   
   validates_uniqueness_of :short_code, :on => :create, :message => "must be unique"
   validates_format_of :short_code, :with => /^[A-Za-z0-9]{1,5}$/, :on => :create, :message => "is not of the correct format. It must be between one and five letters and numbers without spaces or symbols."
-  
+
   def slots
     Slot.find_all_by_room_id(id)
   end
