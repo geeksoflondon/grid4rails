e708ef16b05c0c3dfe3926c3b96e513ae8771396
Kevin Prince <nextgengames@gmail.com>
2011-08-07 00:57:48 +0100

    Added default scoping to timeslots.

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,6 +1,7 @@
 class Timeslot < ActiveRecord::Base
   validates :end, :uniqueness => {:scope => :start}
-  
+  default_scope order('start')
+
   def self.non_assignables
     Timeslot.where("assign_slots = ?", false)
   end
