f5f4c6415be6dc021d3f05feaace3ebc5cb69917
martian-a <coder@bluegumtree.com>
2011-08-11 04:02:13 +0100

    Updating grid views with Morena's styling, split into three versions.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// TEMP: Version Switcher
+function setVersion(selected) {
+	var expires = new Date();
+	expires.setDate(expires.getDate() + 3);
+	var value = escape(selected) + "; expires=" + expires.toUTCString();
+	document.cookie = "version=" + value;
+}
