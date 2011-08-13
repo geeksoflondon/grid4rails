72ca20b7a3fe5760375e725aa224736e4e6b275a
Kevin Prince <nextgengames@gmail.com>
2011-08-13 15:06:00 +0100

    grid is now default route to avoid additional http requests.

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ Griddy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "grid#index"
 
   # See how all your routes lay out with "rake routes"
 
