0a086ab43b7bdbfc9bd3e9828d83bcc5378b81e6
martian-a <coder@bluegumtree.com>
2011-09-01 20:46:47 +0100

    Implementing cookie detection.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+function setCookiesAvail() {
+	var expires = new Date();
+	expires.setDate(expires.getDate() + 3);
+	var value = escape("true") + "; expires=" + expires.toUTCString() + ";path=/";
+	document.cookie = "avail=" + value;	
+}
+
 // TEMP: Version Switcher
 function setVersion(selected) {
 	var expires = new Date();
