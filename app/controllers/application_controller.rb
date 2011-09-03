16ec7cc738f6e55ee0c6d52d6a5b7f440d8ae98f
Kevin Prince <nextgengames@gmail.com>
2011-09-04 00:09:12 +0100

    moved cookie detection into the controller. tidy up in header/footer moving asset delivery to cloudfront for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   include Clearance::Authentication
   protect_from_forgery
-  
+
   before_filter :talks_taking_place
-  
+
   def talks_taking_place
     @talks_taking_place = Timeslot.now.nil?
   end
-  
+
+  def which_stylesheet
+    @style = cookies[:version]
+  end
+
 end
