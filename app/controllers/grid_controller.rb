6d1b57fc644822cd6f9cc6b5265cbad9764c84e0
Kevin Prince <nextgengames@gmail.com>
2011-09-03 16:07:41 +0100

    Adding some varnish caching

--- app/controllers/grid_controller.rb
+++ app/controllers/grid_controller.rb
@@ -1,34 +1,46 @@
 class GridController < ApplicationController
 
-  def index    
+  def index
     @page_id = "grid"
     @timeslots = Timeslot.all
     @rooms = Room.all
     @description = "All talks."
+
+    response.headers['Cache-Control'] = 'public, max-age=1' #Storing in varnish for 1 second
+
   end
-    
+
   def now
     @page_id = "now"
-    @timeslot = Timeslot.now    
-    @timeslots = Array.wrap(@timeslot) 
+    @timeslot = Timeslot.now
+    @timeslots = Array.wrap(@timeslot)
     @rooms = Array.wrap(Room.all)
     @description = "What's on now."
+
+    response.headers['Cache-Control'] = 'public, max-age=30' #Storing in varnish for 30 seconds
+
   end
-  
+
   def next
     @page_id = "next"
     @timeslot = Timeslot.next
     @timeslots = Array.wrap(@timeslot)
     @rooms = Array.wrap(Room.all)
     @description = "What's on next."
+
+    response.headers['Cache-Control'] = 'public, max-age=30' #Storing in varnish for 30 seconds
+
   end
-  
+
   def show
     @page_id = "timeslot"
     @timeslot = Timeslot.find(params[:id])
     @timeslots = Array.wrap(@timeslot)
     @rooms = Array.wrap(Room.all)
     @description = "At this time."
+
+    response.headers['Cache-Control'] = 'public, max-age=30' #Storing in varnish for 30 seconds
+
   end
 
 end
