52d3c74455cd45fbbbd1ab2a8b7ed258e6486c26
martian-a <coder@bluegumtree.com>
2011-09-09 14:37:03 +0100

    Updated tests to match changes to .now and .next (.on_now, .on_next)

--- test/unit/slot_test.rb
+++ test/unit/slot_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SlotTest < ActiveSupport::TestCase
-  test "can get the current slots and the next slots" do
+  test "can get the slots in the current and next timeslots (relative to now)" do
     
     now_ts = Timeslot.all.first
     next_ts = Timeslot.all.second
@@ -10,8 +10,8 @@ class SlotTest < ActiveSupport::TestCase
     Timecop.travel(now_ts.start)
     
     
-    assert Slot.now.first.timeslot_id == now_ts.id
-    assert Slot.next.first.timeslot_id == next_ts.id
+    assert Slot.on_now.first.timeslot_id == now_ts.id
+    assert Slot.on_next.first.timeslot_id == next_ts.id
     
     Timecop.return()
     
