28cf5f302a0931449e42e3be29c815172d56df6c
martian-a <coder@bluegumtree.com>
2011-09-19 15:23:39 +0100

    Converting fixnum to string.

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -24,17 +24,17 @@ class Slot < ActiveRecord::Base
 
     timeslots = Timeslot.order(:start).all
 
-	puts "Total timeslots: " + timeslots.count
+	puts "Total timeslots: " + timeslots.count.to_s
 
     Slot.delete_all
 
     timeslots.each do |timeslot|
     
-      puts "Timeslot: " + timeslot.id
+      puts "Timeslot: " + timeslot.id.to_s
     
       Room.all.each do |room|
         
-        puts "Room: " + room.id
+        puts "Room: " + room.id.to_s
         
         Slot.create(:room_id => room.id, :timeslot_id => timeslot.id)        
         
