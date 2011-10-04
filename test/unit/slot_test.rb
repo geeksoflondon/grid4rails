91e0bc5aed49c0a251ed79d9c557fe807cd1adb4
martian-a <coder@bluegumtree.com>
2011-10-04 22:04:27 +0100

    Added a test for creating a slot.

--- test/unit/slot_test.rb
+++ test/unit/slot_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 class SlotTest < ActiveSupport::TestCase
+
+  test "can create a new slot" do
+  
+  	new_slot = Slot.create(:room_id => 1, :timeslot_id => 1)
+  	
+  	assert new_slot.is_a Slot  
+  
+  end
+
   test "can get the slots in the current and next timeslots (relative to now)" do
     
     now_ts = Timeslot.all.first
@@ -8,8 +17,7 @@ class SlotTest < ActiveSupport::TestCase
     
     #Start the day
     Timecop.travel(now_ts.start)
-    
-    
+        
     assert Slot.on_now.first.timeslot_id == now_ts.id
     assert Slot.on_next.first.timeslot_id == next_ts.id
     
