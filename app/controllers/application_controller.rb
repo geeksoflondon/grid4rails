0382686e712b74a2a5b5db7a2f298f26fc785a2c
Kevin Prince <nextgengames@gmail.com>
2011-10-11 22:04:30 +0100

    General visit to the dentist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
-  
-  include Clearance::Authentication
-  
+
   protect_from_forgery
-  
+
   before_filter :enable_cors
   before_filter :talks_taking_place
   before_filter :version
@@ -11,31 +9,31 @@ class ApplicationController < ActionController::Base
   # Flag for checking whether a timeslot matching now, exists.
   # Equals false if no timeslot matching now exists; otherwise will be true.
   def talks_taking_place
-  	@talks_taking_place = Timeslot.on_now.nil? == true ? false : true 
+    @talks_taking_place = Timeslot.on_now.nil? == true ? false : true 
   end
 
   def version
-  	if (!cookies[:version] || cookies[:version].blank?)
-  	 	cookies[:version_check] = true  	
-  	end 
-  	if (cookies[:version] && !cookies[:version].blank?)  
-    	@version = cookies[:version] unless (cookies[:version] != "low" && cookies[:version] != "med" && cookies[:version] != "high") 	
+    if (!cookies[:version] || cookies[:version].blank?)
+      cookies[:version_check] = true
+    end
+    if (cookies[:version] && !cookies[:version].blank?)
+      @version = cookies[:version] unless (cookies[:version] != "low" && cookies[:version] != "med" && cookies[:version] != "high")
     end
     @version ||= "low"
     if (cookies[:version] != @version)
-    	cookies[:version] = @version
-    end    
+      cookies[:version] = @version
+    end
   end
-  
+
   def set_version
     cookies[:version_check] = false
     @version = params[:version] unless (params[:version] != "low" && params[:version] != "med" && params[:version] != "high")
     @version ||= "low"
     cookies[:version] = @version
     if (params[:url])
-        redirect_to params[:url]	
-    else 
-    	redirect_to "/"
+        redirect_to params[:url]
+    else
+      redirect_to "/"
     end
   end
 
@@ -43,15 +41,15 @@ class ApplicationController < ActionController::Base
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET"
   end
-  
-  private
-  
-	before_filter :instantiate_controller_and_action_names
-	
-	def instantiate_controller_and_action_names
-		@current_action = action_name
-		@current_controller = controller_name
-	end
 
-  
+  private
+
+  before_filter :instantiate_controller_and_action_names
+
+  def instantiate_controller_and_action_names
+    @current_action = action_name
+    @current_controller = controller_name
+  end
+
+
 end
