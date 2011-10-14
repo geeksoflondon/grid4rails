b4f1828d23c300cd8cce99a80c4316d8ed3fd433
Kevin Prince <nextgengames@gmail.com>
2011-10-14 21:50:29 +0100

    Changed to be more XML like

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,7 +3,7 @@ class StatsController < ApplicationController
   def talks
     @talks = Talk.count - Slot.where('locked' => true).count
     @slots_free = Slot.where('talk_id' => nil).count
-    render :layout => false
+    render :layout => false, :content_type => 'application/xml'
   end
 
 end
