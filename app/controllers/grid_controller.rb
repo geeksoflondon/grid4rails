e303e98a5c39b9901e94993b11e56ffb762c7cf7
Kevin Prince <nextgengames@gmail.com>
2011-10-15 20:28:19 +0100

    General whitespace and code tidy

--- app/controllers/grid_controller.rb
+++ app/controllers/grid_controller.rb
@@ -1,17 +1,15 @@
 class GridController < ApplicationController
 
-  before_filter :load_grid
-
   def now
     @page_id = "now"
-    @timeslot = Timeslot.on_now    
-	@date = @timeslot.start.to_date unless @timeslot.nil?
+    @timeslot = Timeslot.on_now
+    @date = @timeslot.start.to_date unless @timeslot.nil?
     flash.keep
-	if @date.nil?
-		flash[:warning] = "There's nothing on right now."
-    	redirect_to :controller => "grid", :action => "date", :version => params[:version]
-    else 
-    	redirect_to :controller => "grid", :action => "show", :date => @date, :timeslot => @timeslot.id, :version => params[:version]
+    if @date.nil?
+      flash[:warning] = "There's nothing on right now."
+      redirect_to :controller => "grid", :action => "date", :version => params[:version]
+    else
+      redirect_to :controller => "grid", :action => "show", :date => @date, :timeslot => @timeslot.id, :version => params[:version]
     end
   end
 
@@ -20,11 +18,11 @@ class GridController < ApplicationController
     @timeslot = Timeslot.on_next
     @date = @timeslot.start.to_date unless @timeslot.nil?
     flash.keep
-    if (@date.nil?) 
-    	flash[:warning] = "That's all folks!"
-    	redirect_to :controller => "grid", :action => "date", :version => params[:version]
+    if (@date.nil?)
+      flash[:warning] = "That's all folks!"
+      redirect_to :controller => "grid", :action => "date", :version => params[:version]
     else
-    	redirect_to :controller => "grid", :action => "show", :date => @date, :timeslot => @timeslot.id, :version => params[:version]
+      redirect_to :controller => "grid", :action => "show", :date => @date, :timeslot => @timeslot.id, :version => params[:version]
     end
   end
 
@@ -44,16 +42,20 @@ class GridController < ApplicationController
   def date
     @page_id = "date-grid"
     @show_room_col = true
+
     if (params[:date])
-    	@timeslots = Timeslot.by_date(params[:date])
+      @timeslots = Timeslot.by_date(params[:date])
     else
-    	@timeslots = Timeslot.auto_date
+      @timeslots = Timeslot.auto_date
     end
+
     @date = @timeslots.first.start.to_date
-    if (params[:date].nil?) 
-    	flash.keep
-    	redirect_to :controller => "grid", :action => "date", :date => @date, :version => params[:version]
+
+    if (params[:date].nil?)
+      flash.keep
+      redirect_to :controller => "grid", :action => "date", :date => @date, :version => params[:version]
     end
+
     @dates = Array.wrap(Timeslot.dates)
     @scroller_date = true
     @is_single_timeslot = (@timeslots.count == 1)
@@ -61,18 +63,12 @@ class GridController < ApplicationController
     @rooms = Room.all
     @description = "All talks."
   end
-  
+
 
   def room
-  	@page_id = "room-grid"
-  	flash.keep
+    @page_id = "room-grid"
+    flash.keep
     redirect_to :controller => "rooms", :action => "show", :room => params[:room], :version => params[:version]
-  end
-
-  private
-
-  def load_grid
-    @grid = Grid.new
-  end
+  endf
 
 end
