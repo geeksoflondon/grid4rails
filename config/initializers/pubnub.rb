b1ca039845518788b47cbd28e9d4c95c5f5a137f
martian-a <coder@bluegumtree.com>
2011-10-21 10:58:48 +0100

    Added environment condition to pubnub initializer.

--- config/initializers/pubnub.rb
+++ config/initializers/pubnub.rb
@@ -1,11 +1,13 @@
-publish_key = ENV['PUBNUB_PUBLISH_KEY'];
-subscribe_key = ENV['PUBNUB_SUBSCRIBE_KEY'];
-
-PUBNUB_CHANNEL = 'griddy'.freeze
-
-PUBNUB = Pubnub.new(
-    publish_key,  ## PUBLISH_KEY
-    subscribe_key,  ## SUBSCRIBE_KEY
-    "",      ## SECRET_KEY
-    false    ## SSL_ON?
-)
+if (:environment == 'production')
+	publish_key = ENV['PUBNUB_PUBLISH_KEY'];
+	subscribe_key = ENV['PUBNUB_SUBSCRIBE_KEY'];
+	
+	PUBNUB_CHANNEL = 'griddy'.freeze
+	
+	PUBNUB = Pubnub.new(
+	    publish_key,  ## PUBLISH_KEY
+	    subscribe_key,  ## SUBSCRIBE_KEY
+	    "",      ## SECRET_KEY
+	    false    ## SSL_ON?
+	)
+end
