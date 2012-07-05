d7902e6da32568825e9470e80b2f2195e6beb458
martian-a <coder@bluegumtree.com>
2012-07-05 21:40:31 +0100

    Added environment variable for disabling pubnub (DISABLE_PUBNUB).

--- config/initializers/pubnub.rb
+++ config/initializers/pubnub.rb
@@ -5,17 +5,23 @@ subscribe_key = ENV['PUBNUB_SUBSCRIBE_KEY'] || 'demo'
 secret_key    = ENV['PUBNUB_SECRET_KEY'] || ''
 ssl_on        = false
 
-PUBNUB_CHANNEL = 'griddy'
+if ENV["DISABLE_PUBNUB"] != "true"
+  
+  puts('Creating new PubNub Client API')
 
-## -----------------------------------------
-## Create Pubnub Client API (INITIALIZATION)
-## -----------------------------------------
-
-puts('Creating new PubNub Client API')
-
-PUBNUB = Pubnub.new(
-    publish_key,
-    subscribe_key,
-    secret_key,
-    ssl_on
-)
+  ## -----------------------------------------
+  ## Create Pubnub Client API (INITIALIZATION)
+  ## -----------------------------------------
+  
+  PUBNUB_CHANNEL = 'griddy'
+  
+  PUBNUB = Pubnub.new(
+      publish_key,
+      subscribe_key,
+      secret_key,
+      ssl_on
+  )  
+  
+else
+  puts("Pubnub disabled")
+end 
