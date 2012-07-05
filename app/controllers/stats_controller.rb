36c23c6c82fdd2928d59c5300348181ab5025a60
Kevin Prince <nextgengames@gmail.com>
2012-07-05 13:55:46 +0100

    switched stats to json

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,38 +1,40 @@
 class StatsController < ActionController::Base
 
-	def talks
-		@talks = Talk.count - Slot.where('locked' => true).count
-		@slots_free = Slot.where('talk_id' => nil).count
-		render :layout => false, :content_type => 'application/xml'
-	end
+  def talks
+    @talks = Talk.count - Slot.where('locked' => true).count
+    @slots_free = Slot.where('talk_id' => nil).count
+    result = {:talks => @talks, :slots_free => @slots_free}
+    render :json => result.to_json
+  end
 
   def time_till
     t = Timeslot.on_next
     minute = (t.start - Time.now)/60
     @minute = minute.round
-    render :layout => false, :content_type => 'application/xml'
+    result = {:minute => @minute}
+    render :json => result.to_json
   end
 
-	def now
-		@page_id = "now"
-		@timeslot = Timeslot.on_now
-		@date = @timeslot.start.to_date unless @timeslot.nil?
-		if @date.nil?
-			#Nothing on Yet
-		else
-			#Whats on now
-		end
-	end
+  def now
+    @page_id = "now"
+    @timeslot = Timeslot.on_now
+    @date = @timeslot.start.to_date unless @timeslot.nil?
+    if @date.nil?
+      #Nothing on Yet
+    else
+      #Whats on now
+    end
+  end
 
-	def next
-		@page_id = "next"
-		@timeslot = Timeslot.on_next
-		@date = @timeslot.start.to_date unless @timeslot.nil?
-		if (@date.nil?)
-			#Thats all folks
-		else
-			#Whats coming up
-		end
-	end
+  def next
+    @page_id = "next"
+    @timeslot = Timeslot.on_next
+    @date = @timeslot.start.to_date unless @timeslot.nil?
+    if (@date.nil?)
+      #Thats all folks
+    else
+      #Whats coming up
+    end
+  end
 
 end
